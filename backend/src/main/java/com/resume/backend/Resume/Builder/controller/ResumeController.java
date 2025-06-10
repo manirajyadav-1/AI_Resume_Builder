a58eca40cd6f15976d51d85b4d96c6eeca054e54
manirajyadav-1 <yadavmani41@gmail.com>
2025-06-10 21:51:32 +0530

    Added login success and logout endpoints

--- backend/src/main/java/com/resume/backend/Resume/Builder/controller/ResumeController.java
+++ backend/src/main/java/com/resume/backend/Resume/Builder/controller/ResumeController.java
@@ -2,15 +2,17 @@ package com.resume.backend.Resume.Builder.controller;
 
 import com.resume.backend.Resume.Builder.ResumeRequest;
 import com.resume.backend.Resume.Builder.service.ResumeService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
 import java.util.Map;
 
 @RestController
-@CrossOrigin("*")
 @RequestMapping("/api/v1/resume")
 public class ResumeController {
 
@@ -32,4 +34,16 @@ public class ResumeController {
         }
     }
 
+    @GetMapping("/success")
+    public ResponseEntity<String> loginSuccess(@AuthenticationPrincipal OAuth2User principal){
+        return ResponseEntity.ok(principal.getAttribute("email"));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.getSession().invalidate();
+        response.setHeader("Set-Cookie", "JSESSIONID=; HttpOnly; Path=/; Max-Age=0; SameSite=None; Secure");
+
+        return ResponseEntity.ok("Logged out successfully");
+    }
 }
