34309bdaa29e142a8aeaa4e52b013a726c0ce974
manirajyadav-1 <yadavmani41@gmail.com>
2025-06-11 21:30:09 +0530

    add name field for signup

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
+import { useAuth } from "../context/AuthContext";
 
 const LoginSignup = () => {
+  const { signup, login } = useAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isSignup, setIsSignup] = useState(true);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Email:", email);
-    console.log("Password:", password);
+    if (isSignup) {
+      await signup(email, password);
+    } else {
+      await login(email, password);
+    }
   };
 
-
   return (
-    <div className="py-[-10px]">
-      <div className="container mx-auto flex flex-col pt-12 my-5 rounded-lg">
+    <div className="mt-[-120px]">
+      <div className="container mx-auto flex flex-col pt-12 my-4 rounded-lg">
         <div className="flex justify-center w-full h-full my-auto xl:gap-14 md:gap-5">
           <div className="flex items-center justify-center w-full lg:p-12">
             <div className="flex items-center xl:p-10">
@@ -21,8 +26,24 @@ const LoginSignup = () => {
                 onSubmit={handleSubmit}
                 className="w-full h-full flex flex-col pb-6 text-center rounded-3xl"
               >
-                <h3 className="mb-3 text-4xl font-extrabold text-dark-grey-900">Sign In</h3>
+                <h3 className="mb-3 text-4xl font-extrabold text-dark-grey-900">{isSignup ? "Sign Up" : "Sign In"}</h3>
                 <p className="mb-4 text-grey-700">Enter your email and password</p>
+
+                {/* Name Field */}
+                {isSignup && <>
+                  <label htmlFor="email" className="mb-2 text-sm text-start text-grey-900">
+                    Name*
+                  </label>
+                  <input
+                    id="name"
+                    type="name"
+                    placeholder="John Doe"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full px-5 py-4 mb-4 text-sm font-medium border bg-grey-200 text-dark-grey-900 rounded-2xl outline-none focus:bg-grey-400 placeholder:text-grey-700"
+                    required
+                  />
+                </>}
 
                 {/* Email Field */}
                 <label htmlFor="email" className="mb-2 text-sm text-start text-grey-900">
@@ -34,7 +55,7 @@ const LoginSignup = () => {
                   placeholder="johndoe@gmail.com"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  className="w-full px-5 py-4 mb-7 text-sm font-medium border bg-grey-200 text-dark-grey-900 rounded-2xl outline-none focus:bg-grey-400 placeholder:text-grey-700"
+                  className="w-full px-5 py-4 mb-4 text-sm font-medium border bg-grey-200 text-dark-grey-900 rounded-2xl outline-none focus:bg-grey-400 placeholder:text-grey-700"
                   required
                 />
 
@@ -48,7 +69,7 @@ const LoginSignup = () => {
                   placeholder="Enter a password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="w-full px-5 py-4 mb-5 text-sm font-medium bg-grey-200 border text-dark-grey-900 rounded-2xl outline-none focus:bg-grey-400 placeholder:text-grey-700"
+                  className="w-full px-5 py-4 mb-6 text-sm font-medium bg-grey-200 border text-dark-grey-900 rounded-2xl outline-none focus:bg-grey-400 placeholder:text-grey-700"
                   required
                 />
 
@@ -57,16 +78,21 @@ const LoginSignup = () => {
                   type="submit"
                   className="w-full px-6 py-5 mb-5 text-sm bg-base-300 font-bold text-white transition duration-300 rounded-2xl hover:bg-purple-blue-600 focus:ring-4 focus:ring-purple-blue-100 bg-purple-blue-500 md:w-96"
                 >
-                  Sign In
+                  {isSignup ? "Sign Up" : "Sign In"}
                 </button>
 
                 {/* Footer Link */}
-                <p className="text-sm text-grey-900 mb-5">
+                {!isSignup ? <p className="text-sm text-grey-900 mb-5">
                   Not registered yet?{" "}
-                  <a href="#" className="font-bold text-grey-700">
+                  <a className="font-bold text-grey-700 cursor-pointer" onClick={() => setIsSignup(true)}>
                     Create an Account
                   </a>
-                </p>
+                </p> : <p className="text-sm text-grey-900 mb-5">
+                  Already registered?{" "}
+                  <a className="font-bold text-grey-700 cursor-pointer" onClick={() => setIsSignup(false)}>
+                    Login
+                  </a>
+                </p>}
 
                 {/* Divider */}
                 <div className="flex items-center mb-5">
@@ -75,36 +101,38 @@ const LoginSignup = () => {
                   <hr className="h-0 border-b border-solid border-grey-500 grow" />
                 </div>
 
-                {/* Google Sign-In */}
-                <a href="http://localhost:8080/oauth2/authorization/google">
-                  <button
-                  type="button"
-                  className="flex items-center justify-center bg-base-300  py-4 mb-2 text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300"
-                  >
-                    <img
-                      src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/logos/logo-google.png"
-                      alt="Google"
-                      className="h-5 mr-2"
-                    />
-                    Sign in with Google
-                  </button>
-                </a>
-                
-
-                {/* GitHub Sign-In (optional) */}
-                <a href="http://localhost:8080/oauth2/authorization/github">
-                  <button
+                <div className="flex gap-8">
+                  {/* Google Sign-In */}
+                  <a href="http://localhost:8080/oauth2/authorization/google" className="flex items-center justify-center">
+                    <button
                     type="button"
-                    className="flex items-center justify-center py-4 text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300"
-                  >
-                    <img
-                      src="https://www.svgrepo.com/show/303615/github-icon-1-logo.svg"
-                      alt="GitHub"
-                      className="h-5 mr-2"
-                    />
-                    Sign in with GitHub
-                  </button>
-                </a>
+                    className="flex items-center justify-center bg-base-300 p-4 mb-2 text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300"
+                    >
+                      <img
+                        src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/logos/logo-google.png"
+                        alt="Google"
+                        className="h-5 mr-2"
+                      />
+                      Sign in with Google
+                    </button>
+                  </a>
+                  
+
+                  {/* GitHub Sign-In */}
+                  <a href="http://localhost:8080/oauth2/authorization/github">
+                    <button
+                      type="button"
+                      className="flex items-center justify-center bg-base-300 p-4 mb-2 text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300"
+                    >
+                      <img
+                        src="https://www.svgrepo.com/show/303615/github-icon-1-logo.svg"
+                        alt="GitHub"
+                        className="h-5 mr-2 bg-gray-300 rounded-full"
+                      />
+                      Sign in with GitHub
+                    </button>
+                  </a>
+                </div>
               </form>
             </div>
           </div>
