f7ac63dfa3ce5d3b604a2a1de3f48f28f6326998
manirajyadav-1 <yadavmani41@gmail.com>
2025-06-13 22:01:16 +0530

    Add method to check if user exists by email in UserRepository

--- backend/src/main/java/com/resume/backend/Resume/Builder/respository/UserRepository.java
+++ backend/src/main/java/com/resume/backend/Resume/Builder/respository/UserRepository.java
@@ -2,8 +2,12 @@ package com.resume.backend.Resume.Builder.respository;
 
 import com.resume.backend.Resume.Builder.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
