20573520915dcb78a1ff1bb3f5286b3f63fe9eb6
manirajyadav-1 <yadavmani41@gmail.com>
2025-06-17 20:59:55 +0530

    add resume storage service

--- backend/src/main/java/com/resume/backend/Resume/Builder/service/ResumeStorageService.java
+++ backend/src/main/java/com/resume/backend/Resume/Builder/service/ResumeStorageService.java
@@ -1,2 +1,17 @@
-package com.resume.backend.Resume.Builder.service;public interface ResumeStorageService {
+package com.resume.backend.Resume.Builder.service;
+
+import com.resume.backend.Resume.Builder.model.Resume;
+import com.resume.backend.Resume.Builder.respository.ResumeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResumeStorageService {
+
+    @Autowired
+    private ResumeRepository resumeRepository;
+
+    public void save(Resume resume){
+        resumeRepository.save(resume);
+    }
 }
