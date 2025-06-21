ca0c7f78f9c6d21158176a99a9ec125d39a880e9
manirajyadav-1 <yadavmani41@gmail.com>
2025-06-21 22:58:25 +0530

    add findByUserId function

--- backend/src/main/java/com/resume/backend/Resume/Builder/respository/ResumeRepository.java
+++ backend/src/main/java/com/resume/backend/Resume/Builder/respository/ResumeRepository.java
@@ -2,12 +2,14 @@ package com.resume.backend.Resume.Builder.respository;
 
 import com.resume.backend.Resume.Builder.model.Resume;
 import com.resume.backend.Resume.Builder.model.User;
+import org.antlr.v4.runtime.misc.MultiMap;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ResumeRepository extends JpaRepository<Resume, Long> {
-    List<Resume> findByUser(User user);
+    Optional<Resume> findByUserId(Long userId);
 }
