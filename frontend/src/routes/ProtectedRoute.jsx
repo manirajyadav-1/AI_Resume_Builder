f2e24b0019e8ac4909e5fdc64067a693036c69e6
manirajyadav-1 <yadavmani41@gmail.com>
2025-06-26 23:01:08 +0530

    restrict access to dashboard and resume routes using ProtectedRout

---
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) return <div className="text-center mt-10">Loading...</div>;
+
+  return isAuthenticated ? children : <Navigate to="/" />;
+};
+
+export default ProtectedRoute;
