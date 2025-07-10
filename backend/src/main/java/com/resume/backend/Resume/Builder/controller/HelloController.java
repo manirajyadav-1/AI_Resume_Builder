45562c28184fc4963391f611605d05bf8f52b75f
manirajyadav-1 <yadavmani41@gmail.com>
2025-07-10 23:25:20 +0530

    Add root endpoint returning backend status message

---
+++ backend/src/main/java/com/resume/backend/Resume/Builder/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.resume.backend.Resume.Builder.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello(){
+        return "Hello! Backend Services Started";
+    }
+}
