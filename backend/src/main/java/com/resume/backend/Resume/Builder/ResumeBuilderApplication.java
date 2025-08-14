dae4c286992016d045bf90705b37deecb01ce585
manirajyadav-1 <yadavmani41@gmail.com>
2025-08-14 22:46:59 +0530

    remove logenv function

--- backend/src/main/java/com/resume/backend/Resume/Builder/ResumeBuilderApplication.java
+++ backend/src/main/java/com/resume/backend/Resume/Builder/ResumeBuilderApplication.java
@@ -1,15 +1,12 @@
 package com.resume.backend.Resume.Builder;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
 
 
 @SpringBootApplication
@@ -34,11 +31,5 @@ public class ResumeBuilderApplication {
 						.allowCredentials(true);
 			}
 		};
-	}
-
-	@PostConstruct
-	public void logEnvVars() {
-		System.out.println("DB URL: " + System.getenv("DATASOURCE_URL"));
-		System.out.println("DB USER: " + System.getenv("DATASOURCE_USER"));
 	}
 }
